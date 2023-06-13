ab1d01c5e76fc9844eca4f4f579bef7606e4b71d
harsh-theexponent <81282949+harsh-theexponent@users.noreply.github.com>
2023-06-13 11:26:44 +0530

    localstorage use

---
+++ pages/_app.tsx
@@ -0,0 +1,14 @@
+import '@/styles/globals.css'
+import type { AppProps } from 'next/app'
+import {appWithI18Next,useSyncLanguage} from "ni18n";
+import {ni18nConfig} from "../ni18n.config";
+
+const App=({ Component, pageProps }: AppProps) =>{
+  const locale =
+  typeof window !== 'undefined' &&  window.localStorage.getItem('MY_LANGUAGE') || 'np'
+
+useSyncLanguage(locale)
+  return <Component {...pageProps} />
+}
+
+export default appWithI18Next(App, ni18nConfig)
