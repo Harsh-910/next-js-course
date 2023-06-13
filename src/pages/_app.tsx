69c87d32b33d397d7f2280fb460168e2a5e84244
Harsh Chhabra <harshchhabra910@gmail.com>
2023-06-13 06:48:44 +0000

    working

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,7 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
 import {appWithI18Next,useSyncLanguage} from "ni18n";
-import {ni18nConfig} from "../ni18n.config";
+import {ni18nConfig} from "../../ni18n.config";
 
 const App=({ Component, pageProps }: AppProps) =>{
   const locale =
