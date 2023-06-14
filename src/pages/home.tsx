41416161172401c26a85c2866236c5abf3ac1946
Harsh Chhabra <harshchhabra910@gmail.com>
2023-06-14 17:55:54 +0000

    routing completed

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,7 +1,17 @@
+import Link from 'next/link'
+import {useRouter} from 'next/router';
+
 const Home = () => {
+    const router = useRouter();
+
+    const changeRoute=()=>{
+        router.push('/about');
+    }
     return (
         <>
             <h2>Home</h2>
+            <Link href='/about'>About thorugh Link</Link>
+            <button onClick={changeRoute}>About</button>
         </>
     )
 }
